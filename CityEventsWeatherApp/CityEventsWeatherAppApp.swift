0229fa0764da91e995405201907c0790c8a239a6
Arman <arman.mohiuddin79@myhunter.cuny.edu>
2025-11-06 13:32:37 -0500

    Initial Commit

---
+++ CityEventsWeatherApp/CityEventsWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CityEventsWeatherAppApp.swift
+//  CityEventsWeatherApp
+//
+//  Created by Arman Mohiuddin on 11/6/25.
+//
+
+import SwiftUI
+
+@main
+struct CityEventsWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
